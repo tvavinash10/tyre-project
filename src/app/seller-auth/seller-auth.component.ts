c0578fbcf81b539c377ee2ee6ddc72a05820d444
Avinash.T.V <tvavinash10@gmail.com>
2022-12-12 22:47:08 +0530

    Integrate Sign-up API for seller

--- src/app/seller-auth/seller-auth.component.ts
+++ src/app/seller-auth/seller-auth.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { signUp } from '../data-type';
+import { SellerService } from '../services/seller.service';
+import { Router } from "@angular/router";
 @Component({
   selector: 'app-seller-auth',
   templateUrl: './seller-auth.component.html',
   styleUrls: ['./seller-auth.component.css']
 })
-export class SellerAuthComponent {
-  signUp(data:object):void{
+export class SellerAuthComponent implements OnInit {
+  constructor(private seller: SellerService, private router: Router) { }
+  ngOnInit(): void {
+
+  }
+  signUp(data: signUp): void {
     console.warn(data);
+    this.seller.userSignUp(data).subscribe((result) => {
+      if (result) {
+        this.router.navigate(['seller-home']);
+      }
+    });
   }
 }

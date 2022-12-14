8c4535bc1cf162d1493b206c6ee5f450570d9821
Avinash.T.V <tvavinash10@gmail.com>
2022-12-14 11:18:52 +0530

    Seller Login Form

--- src/app/seller-auth/seller-auth.component.ts
+++ src/app/seller-auth/seller-auth.component.ts
@@ -8,12 +8,21 @@ import { Router } from "@angular/router";
   styleUrls: ['./seller-auth.component.css']
 })
 export class SellerAuthComponent implements OnInit {
+  showLogin = true;
   constructor(private seller: SellerService, private router: Router) { }
   ngOnInit(): void {
     this.seller.reloadSeller();
   }
   signUp(data: signUp): void {
-    console.warn(data);
     this.seller.userSignUp(data);
+  }
+  login(data: signUp): void {
+    
+  }
+  openLogin(){
+    this.showLogin = true;
+  }
+  openSignup(){
+    this.showLogin = false;
   }
 }

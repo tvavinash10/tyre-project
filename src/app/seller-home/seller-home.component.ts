6dbf32edd5a6b46c3c883181a10da357bee7847e
Avinash.T.V <tvavinash10@gmail.com>
2022-12-21 13:52:20 +0530

    Delete Product with API

--- src/app/seller-home/seller-home.component.ts
+++ src/app/seller-home/seller-home.component.ts
@@ -9,10 +9,28 @@ import { ProductService } from '../services/product.service';
 })
 export class SellerHomeComponent implements OnInit{
   productList:undefined | product [];
+  productMessage: undefined | string;
+  icon = faTrash;
+  iconEdit=faEdit;
   constructor(private product:ProductService){
 
   }
   ngOnInit(): void {
+    this.list();
+  }
+  deleteProduct(id: number) {
+    this.product.deleteProduct(id).subscribe((result) => {
+      if (result) {
+        this.productMessage = 'Product is deleted';
+
+        this.list();
+      }
+    });
+    setTimeout(() => {
+      this.productMessage = undefined;
+    }, 3000);
+  }
+  list(){
     this.product.productList().subscribe((result)=>{
       if(result){
         this.productList=result;

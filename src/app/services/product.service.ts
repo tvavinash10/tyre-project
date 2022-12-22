c2d502c6b2bee826a1ddf501c2547eedf18bcf91
Avinash.T.V <tvavinash10@gmail.com>
2022-12-22 11:49:21 +0530

    UI for Update Product

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -17,4 +17,13 @@ export class ProductService {
   deleteProduct(id: number) {
     return this.http.delete(`http://localhost:3000/products/${id}`);
   }
+  getProduct(id: string) {
+    return this.http.get<product>(`http://localhost:3000/products/${id}`);
+  }
+  updateProduct(product: product) {
+    return this.http.put<product>(
+      `http://localhost:3000/products/${product.id}`,
+      product
+    );
+  }
 }

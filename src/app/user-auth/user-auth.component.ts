d1ed25d3141a23e6622b9ef5dcaac170c746f5dc
Avinash.T.V <tvavinash10@gmail.com>
2022-12-30 11:10:34 +0530

    Sign Up API in angular

--- src/app/user-auth/user-auth.component.ts
+++ src/app/user-auth/user-auth.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { signUp } from '../data-type';
+import { SellerService } from '../services/seller.service';
 
 @Component({
   selector: 'app-user-auth',
   templateUrl: './user-auth.component.html',
   styleUrls: ['./user-auth.component.css']
 })
-export class UserAuthComponent {
+export class UserAuthComponent implements OnInit {
+  constructor(private seller: SellerService){
 
+  }
+  ngOnInit(): void {
+    this.seller.reloadSeller()
+  }
+  signUp(data: signUp): void {
+    this.seller.userSignUp(data);
+  }
 }
